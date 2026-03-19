1405040aeb101311eaf44ae963ee2ffe37815fc9
Taras Karpuk <taras.karpuk@gmail.com>
2026-03-19 10:26:27 -0600

    add CORS demo policy

--- backend/Api/Api/Program.cs
+++ backend/Api/Api/Program.cs
@@ -5,11 +5,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddSingleton<IBookRepository, BookRepository>();
+builder.Services.AddCors(o => o.AddPolicy("demo", p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors("demo");
 app.UseHttpsRedirection();
 
 app.MapGet("/hello", () =>
