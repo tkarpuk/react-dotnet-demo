ba70460839bcdd71ced5586ee1c1af70a942ecf4
Taras Karpuk <taras.karpuk@gmail.com>
2026-03-19 14:50:06 -0600

    display books list

--- frontend/react-app/src/api/BooksApi.js
+++ frontend/react-app/src/api/BooksApi.js
@@ -3,9 +3,17 @@ const BASE_URL = 'http://localhost:5156';
 export async function GetHello() {
     const response = await fetch(`${BASE_URL}/hello`);
     if (!response.ok) {
-        console.error("api:GetHello() - request failed");
-        throw new Error("Request failed");
+        throw new Error("api:GetHello() - request failed");
     }
 
     return await response.text();
-} 
+}
+
+export async function GetBooks() {
+    const response = await fetch(`${BASE_URL}/api/books`);
+    if (!response.ok) {
+        throw new Error("api:GetBooks() - request failed");
+    }
+
+    return await response.json();    
+}
--- frontend/react-app/src/pages/BooksList.jsx
+++ frontend/react-app/src/pages/BooksList.jsx
@@ -1,27 +1,44 @@
+import { useEffect, useState } from "react";
+import { GetBooks } from "../api/BooksApi";
+
 export default function BooksList() {
+    const [books, setBooks] = useState([]);
+    useEffect(()=> {
+        GetBooks()
+        .then(res => setBooks(res))
+        .catch(err => console.error(err));
+    }, []);
 
     return (
         <div>
             <h2>Books List</h2>
             <p><button>Add Book</button></p>
             <table border="" width="80%">
-                <tr>
-                    <th>ID</th>
-                    <th>Title</th>
-                    <th>Author</th>
-                    <th>Year</th>
-                    <th>Actions</th>
-                </tr>
-                <tr>
-                    <td>0</td>
-                    <td>The book</td>
-                    <td>The Author</td>
-                    <td>2025</td>
-                    <td>
-                        <button>Edit</button>{' '}
-                        <button>Delete</button>
-                    </td>
-                </tr>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Year</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                
+                <tbody>
+                    {books.map((b) => (
+                    <tr key={b.id}>
+                        <td>{b.id}</td>
+                        <td>{b.title}</td>
+                        <td>{b.author}</td>
+                        <td>{b.year}</td>
+                        <td>
+                            <button>Edit</button>{' '}
+                            <button>Delete</button>
+                        </td>
+                    </tr>
+                    ))}
+
+                </tbody>
             </table>
         </div>
     );
