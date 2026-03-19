d15594db381872be8f410c67425c11fca5cad407
Taras Karpuk <taras.karpuk@gmail.com>
2026-03-19 11:20:56 -0600

    init empty React app

--- backend/Api/Api/Program.cs
+++ backend/Api/Api/Program.cs
@@ -11,7 +11,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseCors("demo");
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.MapGet("/hello", () =>
 {
---
+++ frontend/react-app/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Hi! This is demo React app.</h1>
+}
+
+export default App
