adf3ab106985e99c502f56d8a4e26347ce19fa4a
Taras Karpuk <taras.karpuk@gmail.com>
2026-03-18 18:54:58 -0600

    init minimal API with hello endpoint

---
+++ backend/Api/Api/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.MapGet("/hello", () =>
+{
+    return "Hello, world!";
+});
+
+app.Run();
