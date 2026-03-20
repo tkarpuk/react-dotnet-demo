31a55849bc22876b64eeb2773fbe32f06769db96
Taras Karpuk <taras.karpuk@gmail.com>
2026-03-20 12:23:41 -0600

    add client validation

--- frontend/react-app/src/pages/BookForm.jsx
+++ frontend/react-app/src/pages/BookForm.jsx
@@ -12,6 +12,7 @@ export default function BookForm() {
         year: 1900,
         price: 0
     });
+    const [errors, setErrors] = useState({});
 
     function loadBook(id) {
         if (!id) return;
@@ -35,14 +36,29 @@ export default function BookForm() {
 
     function handleSubmit(e) {
         e.preventDefault();
+
+        const newErrors = validate();
+        setErrors(newErrors);
+
+        if (Object.keys(newErrors).length > 0) {
+            console.log('Validation errors');
+            return;
+        }
         
         id ? update(id, form) : create(form);
     }
 
+    function validate() {
+        const newErrors = {};
+        if (!form.title?.trim()) newErrors.title = "Title is required";
+        if (!form.author?.trim()) newErrors.author = "Author is required";
+        if (!form.year || (form.year < 1000 && form.year > 2026)) newErrors.year = "Year should be between 1000 and 2026";
+        if (!form.price || form.price <= 0) newErrors.price = "Price must be more than 0";
+
+        return newErrors;
+    }
+
     function create(book) {
-
-        console.log(book);
-
         createBook(book)
             .then(() => goBack())
             .catch(err => console.error(err));
@@ -63,19 +79,27 @@ export default function BookForm() {
                     <form onSubmit={handleSubmit}>
                         <div className="mb-3">
                             <label className="form-label">Title</label> <br />
-                            <input type="text" name="title" onChange={handleChange} value={form.title} className="form-control"/>
+                            <input type="text" name="title" onChange={handleChange} value={form.title} 
+                                className={`form-control ${errors.title ? "is-invalid" : ""}`} />
+                            <div className="invalid-feedback">{errors.title}</div>
                         </div>
                         <div className="mb-3">
                             <label className="form-label">Author</label> <br />
-                            <input type="text" name="author" onChange={handleChange} value={form.author} className="form-control"/>
+                            <input type="text" name="author" onChange={handleChange} value={form.author} 
+                                className={`form-control ${errors.author ? "is-invalid" : ""}`}/>
+                            <div className="invalid-feedback">{errors.author}</div>
                         </div>
                         <div className="mb-3">
                             <label className="form-label">Year</label> <br />
-                            <input type="number" name="year" onChange={handleChange} value={form.year} className="form-control"/>
+                            <input type="number" name="year" onChange={handleChange} value={form.year} 
+                                className={`form-control ${errors.year ? "is-invalid" : ""}`}/>
+                            <div className="invalid-feedback">{errors.year}</div>
                         </div>
                         <div className="mb-3">
                             <label className="form-label">Price</label> <br />
-                            <input type="number" name="price" onChange={handleChange} value={form.price} className="form-control"/>
+                            <input type="number" name="price" onChange={handleChange} value={form.price} 
+                                className={`form-control ${errors.price ? "is-invalid" : ""}`}/>
+                            <div className="invalid-feedback">{errors.price}</div>
                         </div>
                         <div className="d-flex gap-2">
                             <button type="sumbit" className="btn btn-primary">Save</button> {'    '}
