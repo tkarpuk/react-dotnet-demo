3d9383f14e85eeb138fa4945c4f20519f7808d08
Taras Karpuk <taras.karpuk@gmail.com>
2026-03-19 20:06:54 -0600

    add create, update functionality

--- frontend/react-app/src/api/BooksApi.js
+++ frontend/react-app/src/api/BooksApi.js
@@ -18,10 +18,43 @@ export async function getBooks() {
     return await response.json();    
 }
 
+export async function getBookById(id) {
+    const response = await fetch(`${BASE_URL}/api/books/${id}`);
+    if (!response.ok) {
+        throw new Error("api:GetBooks() - request failed");
+    }
+
+    return await response.json();    
+}
+
 export async function deleteBook(id) {
     const response = await fetch(`${BASE_URL}/api/books/${id}`, 
         { method: 'DELETE' });
     if (!response.ok) {
         throw new Error(`api:deleteBook() - request failed id = ${id}`);
+    }  
+}
+
+export async function createBook(book) {
+    const response = await fetch(`${BASE_URL}/api/books`, 
+        { 
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(book)
+        });
+    if (!response.ok) {
+        throw new Error("api:createBook() - request failed");
+    }  
+}
+
+export async function updateBook(id, book) {
+    const response = await fetch(`${BASE_URL}/api/books/${id}`, 
+        { 
+            method: 'PUT',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(book)
+        });
+    if (!response.ok) {
+        throw new Error(`api:updateBook() - request failed id = ${id}`);
     }  
 }
--- frontend/react-app/src/pages/BookForm.jsx
+++ frontend/react-app/src/pages/BookForm.jsx
@@ -1,7 +1,85 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { getBookById, createBook, updateBook } from "../api/BooksApi";
 
 export default function BookForm() {
     const { id } = useParams();
+    const navigate = useNavigate();
+    const [form, setForm] = useState({
+        title: "",
+        author: "",
+        year: 1900,
+        price: 0
+    });
 
-    return <h2>BookForm ({id})</h2>
+    function loadBook(id) {
+        if (!id) return;
+        
+        getBookById(id)
+        .then(res => setForm(res))
+        .catch(err => console.error(err));
+    }
+        
+    useEffect(()=> {
+        loadBook(id);
+    }, [id]);
+
+    function goBack() {
+        navigate("/");
+    }
+
+    function handleChange(e) {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        
+        id ? update(id, form) : create(form);
+    }
+
+    function create(book) {
+
+        console.log(book);
+
+        createBook(book)
+            .then(() => goBack())
+            .catch(err => console.error(err));
+    }
+
+    function update(id, book) {
+        updateBook(id, book)
+            .then(() => goBack())
+            .catch(err => console.error(err));
+    }
+
+    return (
+        <div>
+            <h2>BookForm ({id})</h2>
+            <form onSubmit={handleSubmit}>
+                <p>
+                    <label>Title</label> <br />
+                    <input type="text" name="title" onChange={handleChange} value={form.title}/>
+                </p>
+                <p>
+                    <label>Author</label> <br />
+                    <input type="text" name="author" onChange={handleChange} value={form.author}/>
+                </p>
+                <p>
+                    <label>Year</label> <br />
+                    <input type="number" name="year" onChange={handleChange} value={form.year}/>
+                </p>
+                <p>
+                    <label>Price</label> <br />
+                    <input type="number" name="price" onChange={handleChange} value={form.price}/>
+                </p>
+                <p>
+                    <button type="sumbit">Save</button> {'  '}
+                    <button type="button" onClick={() => goBack()}>Cancel</button>
+                </p>
+            </form>
+        </div>
+
+    )
 }
