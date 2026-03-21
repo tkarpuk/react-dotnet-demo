6bdca1fb01930aace03b36cf1c3d4dec0a3bfd37
Taras Karpuk <taras.karpuk@gmail.com>
2026-03-21 13:07:12 -0600

    add docker-compose and configure frontend

--- frontend/react-app/src/api/BooksApi.js
+++ frontend/react-app/src/api/BooksApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:5156';
+const BASE_URL = ''; // if you run locally - write your backend address and port
 
 export async function getHello() {
     const response = await fetch(`${BASE_URL}/hello`);
